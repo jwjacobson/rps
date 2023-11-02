7b65797d2514ed8df127e480070350e8804cd623
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2023-11-02 16:52:39 -0400

    Add weapon class

--- main.py
+++ main.py
@@ -4,37 +4,45 @@ from rich import print, box
 from rich.prompt import Prompt
 from rich.table import Table
 
+class Weapon:
+    def __init__(self, name, beats):
+        self.name = name
+        self.beats = beats
+
+    def __repr__(self):
+        return f'{self.name} | beats {self.beats}'
+
+
+choices = ['rock', 'paper', 'scissors']
 
 def offering():
-    print('[bold bright_magenta]Choose your weapon:')
-    print(':moai: [bold yellow]rock')
-    print(':newspaper: [bold green]paper')
-    print(':scissors: [bold cyan]scissors')
-
-table = Table(box=box.ASCII, show_edge=False, leading=True)
-table.add_column(':boom-text:')
-table.add_column('[bold magenta]Choose your weapon')
-table.add_row(':mountain:', '[red]rock')
-table.add_row(':memo-text:', '[yellow]paper')
-table.add_row(':kitchen_knife-text:', '[cyan]scissors')
-
-print(table)
+    table = Table(box=box.ASCII, show_edge=False, leading=True)
+    table.add_column(':boom-text:')
+    table.add_column('[bold magenta]Choose your weapon')
+    table.add_row(':mountain:', '[red]rock')
+    table.add_row(':memo-text:', '[yellow]paper')
+    table.add_row(':kitchen_knife-text:', '[cyan]scissors')
+    print('\n')
+    print(table)
+    print('\n')
 
 def player_choice():
-    choice = Prompt.ask("[bold]Choose your weapon")
+    offering()
+    choice = Prompt.ask("[bold]What do you choose?", choices=choices)
     return choice
 
 def computer_choice():
-    return random.choice(["rock", "paper", "scissors"])
+    return random.choice(choices)
 
-# def main(
-#         computer_choice: str = typer.Argument(default_factory=computer_choice),
-#         player_choice: str = typer.Argument(default_factory=player_choice)
-#     ):
-#     print(f'You choose {player_choice}!')
-#     print(f"Computer chooses {computer_choice}!")
+def main(
+        computer_choice: str = typer.Argument(default_factory=computer_choice),
+        player_choice: str = typer.Argument(default_factory=player_choice)
+    ):
+    print(f'You choose {player_choice}!')
+    print(f"Computer chooses {computer_choice}!")
+    if player_choice == computer_choice:
+        print('Draw...')
+    
 
-# if __name__ == "__main__":
-#     typer.run(main)
-
-# offering()
+if __name__ == "__main__":
+    typer.run(main)
