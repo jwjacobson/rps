ed029d6e28d63e0bb6e17f4c39502038fe8c3b47
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2023-11-02 15:52:02 -0400

    Add player_choice function

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 import random
 import typer
+from rich.prompt import Prompt
+
+# def offering():
+#     print('Choose your wea')
 
 def player_choice():
-    pass
+    choice = Prompt.ask("Choose your weapon")
+    return choice
 
 def computer_choice():
     return random.choice(["rock", "paper", "scissors"])
 
-def main(computer_choice: str = typer.Argument(default_factory=computer_choice)):
+def main(
+        computer_choice: str = typer.Argument(default_factory=computer_choice),
+        player_choice: str = typer.Argument(default_factory=player_choice)
+    ):
+    print(f'You choose {player_choice}!')
     print(f"Computer chooses {computer_choice}!")
 
 if __name__ == "__main__":
     typer.run(main)
+
+player_choice()
