56f04c28cb3c4e8bb56132034fe6319520804d11
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2023-11-02 15:43:40 -0400

    Add computer choice function

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+import random
+import typer
 
+def player_choice():
+    pass
 
-print('Hello')
+def computer_choice():
+    return random.choice(["rock", "paper", "scissors"])
+
+def main(computer_choice: str = typer.Argument(default_factory=computer_choice)):
+    print(f"Computer chooses {computer_choice}!")
+
+if __name__ == "__main__":
+    typer.run(main)
